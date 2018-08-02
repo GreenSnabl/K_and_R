7e24377b5a7c4fb21e93d7ba30ce951af15839f7
GreenSnabl <rubensbarr@web.de>
2018-08-02 12:52:07 +0000

    added standard io

---
+++ Chapter_7/standard_input_and_output.c
@@ -0,0 +1,7 @@
+/* The simplest method for reading input and writing output are
+ *
+ * int getchar(void)
+ * void putchar(int)
+ * int printf(char *fmt, ..)
+ *
+ * which all use stdin and stdout	*/
