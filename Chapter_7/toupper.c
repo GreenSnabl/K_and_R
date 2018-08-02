3617d8011ddc1f0084e31c7931dcea43ad56be14
GreenSnabl <rubensbarr@web.de>
2018-08-02 12:55:24 +0200

    added text formating programs

---
+++ Chapter_7/toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(int argc, char** argv)
+{
+	char c;
+
+	if (strcmp(argv[0], "tolower") == 0)
+		while ((c = getchar()) != EOF)
+			putchar(tolower(c));
+	else
+		while ((c = getchar()) != EOF)
+			putchar(toupper(c));
+	return 0;
+}
